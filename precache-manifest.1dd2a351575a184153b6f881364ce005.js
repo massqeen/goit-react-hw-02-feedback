92a07ffdc5277e7493baaf4e806c3727557f8e4b
massqeen <62159695+massqeen@users.noreply.github.com>
2020-09-24 00:18:06 +0300

    Updates

---
+++ precache-manifest.1dd2a351575a184153b6f881364ce005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b6a684424384b7a5355b5ac2a64dcd",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "0fc272167a15b356ca18",
+    "url": "/goit-react-hw-02-feedback/static/css/main.c2869d0a.chunk.css"
+  },
+  {
+    "revision": "b15f79ad01e3657617a1",
+    "url": "/goit-react-hw-02-feedback/static/js/2.745e260f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.745e260f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc272167a15b356ca18",
+    "url": "/goit-react-hw-02-feedback/static/js/main.1e8f0375.chunk.js"
+  },
+  {
+    "revision": "64b4ef9ad6c48c5f73df",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.b6d3dc9f.js"
+  }
+]);
