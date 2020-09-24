54be39567114b0e36ef7e89b6b63fbe07a7d501a
massqeen <62159695+massqeen@users.noreply.github.com>
2020-09-24 23:08:51 +0300

    Updates

--- precache-manifest.bb18e160828b6095ba032253a1701fc0.js
+++ precache-manifest.bb18e160828b6095ba032253a1701fc0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b03387adda80dbb520101720e2c1ff28",
+    "revision": "762f56afbb49422edcc0ad971e610815",
     "url": "/goit-react-hw-02-feedback/index.html"
   },
   {
-    "revision": "dd13fe48d06f9b21428b",
+    "revision": "646d50efc3a3d9ef9cb8",
     "url": "/goit-react-hw-02-feedback/static/css/main.c2869d0a.chunk.css"
   },
   {
-    "revision": "b15f79ad01e3657617a1",
-    "url": "/goit-react-hw-02-feedback/static/js/2.745e260f.chunk.js"
+    "revision": "9060afdd09a8d0c7baed",
+    "url": "/goit-react-hw-02-feedback/static/js/2.f474ebe2.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/goit-react-hw-02-feedback/static/js/2.745e260f.chunk.js.LICENSE.txt"
+    "url": "/goit-react-hw-02-feedback/static/js/2.f474ebe2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dd13fe48d06f9b21428b",
-    "url": "/goit-react-hw-02-feedback/static/js/main.6b34aa4a.chunk.js"
+    "revision": "646d50efc3a3d9ef9cb8",
+    "url": "/goit-react-hw-02-feedback/static/js/main.03bbe9cd.chunk.js"
   },
   {
     "revision": "64b4ef9ad6c48c5f73df",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-feedback/precache-manifest.0d44916dba86253f2264199ffc17c78f.js"
+  "/goit-react-hw-02-feedback/precache-manifest.bb18e160828b6095ba032253a1701fc0.js"
 );
 
 self.addEventListener('message', (event) => {
